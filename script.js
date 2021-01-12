122899dab91c87a0b6075d9259671e2a75a8b400
Glitch (destiny-experienced-lighter) <none>
2021-01-12 02:34:41 +0000

    🎠🏦 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+$(".calculatebutton").click(function() {
+  let name = $(".name").val();
+  let year = $(".year").val();
+  let current_year = new Date().getFullYear() 
+  var age = current_year - year; 
+  $(".message").append(`<p>Hello ${name}, you are ${age} years old.  </p>`);
+}); 
